d9301b3af357cea54ce81219020b498018793f71
Forero <forero.developer01@gmail.com>
2024-09-30 20:39:55 -0500

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+//import BotonMeGusta from "./components/BotonMeGusta"
+
+import TargetQr from "./components/TargetQr"
+
+function App() {
+
+  return (
+   <div>
+    <TargetQr/>
+   </div>
+
+  )
+    
+  
+}
+
+
+export default App
---
+++ src/components/TargetQr.jsx
@@ -0,0 +1,22 @@
+import './TargetQr.css';
+const TargetQr = () => {
+  return (
+    <div className="container-principal">
+      <div className="container-target">
+      <img src="/image-qr-code.png" alt="Logo"/>
+        <div className="textos">
+
+        <h3 className="text1">Improve your front-end skills
+          by building proyects
+        </h3>
+        <p className="text2">Scan the QR code to visit
+          Fronted Mentor and take your coding skills
+          to the next level
+        </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default TargetQr
