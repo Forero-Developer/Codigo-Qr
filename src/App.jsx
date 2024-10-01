da515414729162199a78ada21a97a6f2a5c2077e
Forero <forero.developer01@gmail.com>
2024-10-01 14:32:01 -0500

    Targeta twitter

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,47 @@
 //import BotonMeGusta from "./components/BotonMeGusta"
 
+import { Fragment } from "react";
 import TargetQr from "./components/TargetQr"
-import "./components/cards.css"
+import "./components/components.css"
+import CardTwitter from "./components/cardTwitter";
 
 function App() {
   const qrData = [
     {
       id: 1,
-      text1: "Improve your front-end skills by building projects",
+      text1: "",
       text2: "Scan the QR code to visit Frontend Mentor and take your coding skills to the next level"
     },
     {
       id: 2,
       text1: "Improve your front-end skills by building projects",
-      text2: "Scan the QR code to visit Frontend Mentor and take your coding skills to the next level"
+      Text2: "alberto"
+    },
+    {
+      id: 3,
+      text1: "Improve your front-end skills by building projects",
+      Text2: "alberto"
     }
   ];
 
   return (
-    <div className="cards">
+    <Fragment>
+    <div className="cardqr">
       {qrData.map((qr) => (
         <TargetQr
-          key={qr.id} // Siempre incluye una clave única
-          text1={qr.text1}
-          text2={qr.text2}
+        key={qr.id} // Siempre incluye una clave única
+        text1={qr.text1}
+        text2={qr.text2}
         />
       ))}
     </div>
+
+    <div className="cardtwitter">
+      <CardTwitter/>
+    </div>
+
+      </Fragment>
+    
 
   );
 }
