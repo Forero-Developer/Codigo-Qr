babb280ddc3249233e52771519e6f2f29dd9537d
Forero <forero.developer01@gmail.com>
2024-10-01 10:01:29 -0500

    actu

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,35 @@ import TargetQr from "./components/TargetQr"
 import "./components/cards.css"
 
 function App() {
+  const qrData = [
+    {
+      id: 1,
+      text1: "Improve your front-end skills by building projects",
+      text2: "Scan the QR code to visit Frontend Mentor and take your coding skills to the next level"
+    },
+    {
+      id: 2,
+      text1: "Hola",
+      text2: "Scan the code to visit Frontend Mentor and take your coding skills to the next level"
+    },
+    {
+      id: 3,
+      text1: "oe",
+      text2: "Scan the code to visit Frontend Mentor and take your coding skills to the next level"
+    }
+  ];
 
   return (
-   <div className="cards">
-    <TargetQr
-    text1={"Improve your front-end skills by building proyects"}
-    text2="Scan the QR code to visit Frontend Mentor and take your coding skills to the next level"/>   
-   </div>
-
-
-
-
-
-  )
-    
-  
+    <div className="cards">
+      {qrData.map((qr) => (
+        <TargetQr
+          key={qr.id} // Siempre incluye una clave única
+          text1={qr.text1}
+          text2={qr.text2}
+        />
+      ))}
+    </div>
+  );
 }
 
-
-export default App
+export default App;
